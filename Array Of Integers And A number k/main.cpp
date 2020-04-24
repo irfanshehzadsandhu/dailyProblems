c02703971737cd24c87e89cbade9ab469de9def2
Irfan Shahzad <irfanshehzadsandhu@gmail.com>
2020-04-25 02:28:46 +0500

    Problem #18

---
+++ Array Of Integers And A number k/main.cpp
@@ -0,0 +1,21 @@
+/*This problem was asked by Google.
+
+Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.
+
+For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7, 8, 8], since:
+
+10 = max(10, 5, 2)
+7 = max(5, 2, 7)
+8 = max(2, 7, 8)
+8 = max(7, 8, 7)
+Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results. You can simply print them out as you compute them.
+*/
+
+/* 
+I wrote the sudo code of this problem on copy and fortunately it matches to code mentioned on geeksforgeeks
+GeekforGeeks have also mentioned other two solutions related to BST and Diqueue
+I have paused this problem because I am going to clear the concepts of BigO and I will come back to this later.
+
+More details are available on this link
+https://www.geeksforgeeks.org/sliding-window-maximum-maximum-of-all-subarrays-of-size-k/
+*/
