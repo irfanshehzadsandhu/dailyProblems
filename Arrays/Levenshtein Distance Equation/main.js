84e4f63201e62bc5ccd8685026c16f7fe2c7ed78
Irfan Shahzad <irfanshehzadsandhu@gmail.com>
2020-05-31 19:47:11 +0500

    Problem #31

---
+++ Arrays/Levenshtein Distance Equation/main.js
@@ -0,0 +1,15 @@
+// This problem was asked by Google.
+
+// The edit distance between two strings refers to the minimum number of character
+
+// insertions, deletions, and substitutions required to change one string to the other.
+
+// For example, the edit distance between “kitten” and “sitting” is three:
+
+// substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
+
+// Given two strings, compute the edit distance between them.
+
+//For solution see the following link
+
+//https://medium.com/@ethannam/understanding-the-levenshtein-distance-equation-for-beginners-c4285a5604f0
